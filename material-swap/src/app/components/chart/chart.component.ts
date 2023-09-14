54169a991dfa9ae52ccaec1bfdff66a38ab10131
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-09-14 19:52:33 +0200

    componente chart settato

--- material-swap/src/app/components/chart/chart.component.ts
+++ material-swap/src/app/components/chart/chart.component.ts
@@ -5,6 +5,8 @@ import { isPlatformBrowser } from '@angular/common';
 import * as am5 from '@amcharts/amcharts5';
 import * as am5xy from '@amcharts/amcharts5/xy';
 import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import { IXYChartSettings } from '@amcharts/amcharts5/xy';
+import { IChartData } from 'src/app/interfaces/ichart-data';
 
 @Component({
   selector: 'app-chart',
@@ -198,12 +200,16 @@ export class ChartComponent {
 
     // Impostazione dei colori per le candele in base alla loro variazione
     series.columns.template.states.create("riseFromOpen", {
-      fill: am5.color(0x76b041),
-      stroke: am5.color(0x76b041)
+      //interno candela +
+      fill: am5.color(0xbe6dab),
+      //bordo candela +
+      stroke: am5.color(0xbe6dab)
     });
     series.columns.template.states.create("dropFromOpen", {
-      fill: am5.color(0xe4572e),
-      stroke: am5.color(0xe4572e)
+      //interno candela -
+      fill: am5.color('#1f1739'),
+      //bordo candela -
+      stroke: am5.color('rgb(255, 255, 255)')
     });
     // Personalizzazione del testo del tooltip
     series.get("tooltip")!.label.set("text", "[bold]{valueX.formatDate()}[/]\nOpen: {openValueY}\nHigh: {highValueY}\nLow: {lowValueY}\nClose: {valueY}")
@@ -228,6 +234,116 @@ export class ChartComponent {
 
 
   }
+
+  //INIZIO
+
+  chartInit(onOffPanY: boolean, zoom: IXYChartSettings["wheelY"]): am5xy.XYChart {
+    let chart = this.root.container.children.push(
+      am5xy.XYChart.new(this.root, {
+        // Disabilita il panning sull'asse Y
+        panY: onOffPanY,
+        // Abilita lo zoom sull'asse X usando la rotella del mouse
+        //wheelY: "zoomX",
+        wheelY: zoom,
+        // Utilizza un layout verticale
+        layout: this.root.verticalLayout,
+        // Specifica la distanza massima per il tooltip (0 significa senza limite)
+        maxTooltipDistance: 0
+      })
+    )
+    return chart;
+  }
+
+  insertData(dataForChart: IChartData[]): IChartData[] {
+    let data = dataForChart;
+    return data;
+  }
+
+  xAxisBuild(chart: am5xy.XYChart): am5xy.DateAxis<am5xy.AxisRenderer> {
+    let xAxis = chart.xAxes.push(
+      am5xy.DateAxis.new(this.root, {
+        tooltipDateFormat: "MMM d, yyyy",
+        renderer: am5xy.AxisRendererX.new(this.root, {}),
+        baseInterval: { timeUnit: "day", count: 1 }
+      })
+    );
+    return xAxis;
+  }
+
+  yAxisBuild(chart: am5xy.XYChart): am5xy.ValueAxis<am5xy.AxisRenderer> {
+    let yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(this.root, {
+        renderer: am5xy.AxisRendererY.new(this.root, {})
+      })
+    );
+    return yAxis;
+  }
+
+  candleSeriesCreation(chart: am5xy.XYChart, xAxis: am5xy.DateAxis<am5xy.AxisRenderer>, yAxis: am5xy.ValueAxis<am5xy.AxisRenderer>): am5xy.CandlestickSeries {
+    let series = chart.series.push(
+      am5xy.CandlestickSeries.new(this.root, {
+        name: "Series",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        openValueYField: "open",
+        highValueYField: "high",
+        lowValueYField: "low",
+        valueYField: "close",
+        valueXField: "date",
+        // Impostazione del tooltip per la serie
+        tooltip: am5.Tooltip.new(this.root, {})
+      })
+    );
+    return series;
+  }
+
+  colorCandleSettings(series: am5xy.CandlestickSeries, bodyPlus:string|number, bodyMinus:string|number, borderPlus?:string|number, borderMinus?:string|number){
+    if(borderPlus && borderMinus){
+      series.columns.template.states.create("riseFromOpen", {
+        fill: am5.color(bodyPlus),
+        stroke: am5.color(borderPlus)
+      });
+      series.columns.template.states.create("dropFromOpen", {
+        fill: am5.color(bodyMinus),
+        stroke: am5.color(borderMinus)
+      });
+    } else {
+      series.columns.template.states.create("riseFromOpen", {
+        fill: am5.color(bodyPlus),
+        stroke: am5.color(bodyPlus)
+      });
+      series.columns.template.states.create("dropFromOpen", {
+        fill: am5.color(bodyMinus),
+        stroke: am5.color(bodyMinus)
+      });
+    }
+  }
+
+  tooltip(series: am5xy.CandlestickSeries){
+    series.get("tooltip")!.label.set("text", "[bold]{valueX.formatDate()}[/]\nOpen: {openValueY}\nHigh: {highValueY}\nLow: {lowValueY}\nClose: {valueY}")
+  }
+
+  setPointer(chart: am5xy.XYChart, xAxis: am5xy.DateAxis<am5xy.AxisRenderer>){
+    chart.set("cursor", am5xy.XYCursor.new(this.root, {
+      behavior: "zoomXY",
+      xAxis: xAxis
+    }));
+  }
+
+  confirmSetting(arrayData: IChartData[], xAxis: am5xy.DateAxis<am5xy.AxisRenderer>, yAxis: am5xy.ValueAxis<am5xy.AxisRenderer>, series: am5xy.CandlestickSeries){
+    // Impostazione dei dati per la serie del grafico a candele
+    series.data.setAll(arrayData);
+
+    // Impostazione dei tooltip per gli assi X e Y del grafico
+    xAxis.set("tooltip", am5.Tooltip.new(this.root, {
+      themeTags: ["axis"]
+    }));
+
+    yAxis.set("tooltip", am5.Tooltip.new(this.root, {
+      themeTags: ["axis"]
+    }));
+  }
+
 
 
   browserOnly(f: () => void) {
