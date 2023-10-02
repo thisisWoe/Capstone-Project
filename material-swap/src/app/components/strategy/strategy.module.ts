a46f1cb2ef7b05c4d00ddc98ecfa8f4a25883a4b
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-10-02 15:25:54 +0200

    chart eliminato

--- material-swap/src/app/components/strategy/strategy.module.ts
+++ material-swap/src/app/components/strategy/strategy.module.ts
@@ -3,7 +3,6 @@ import { CommonModule, JsonPipe } from '@angular/common';
 
 import { StrategyRoutingModule } from './strategy-routing.module';
 import { StrategyComponent } from './strategy.component';
-import { ChartComponent } from '../chart/chart.component';
 import { FormsModule } from '@angular/forms';
 import { NgbAlertModule, NgbDatepickerModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -11,7 +10,6 @@ import { NgbAlertModule, NgbDatepickerModule, NgbDropdownModule } from '@ng-boot
 @NgModule({
   declarations: [
     StrategyComponent,
-    ChartComponent,
   ],
   imports: [
     CommonModule,
