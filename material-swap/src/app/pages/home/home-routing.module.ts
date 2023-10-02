ba6c41c92dd59ee302ec3e12610b18abc535b557
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-10-02 15:20:14 +0200

    sign up eliminato

--- material-swap/src/app/pages/home/home-routing.module.ts
+++ material-swap/src/app/pages/home/home-routing.module.ts
@@ -10,7 +10,6 @@ const routes: Routes = [
     { path: 'swap-v2', loadChildren: () => import('./../../components/swap-v2/swap-v2.module').then(m => m.SwapV2Module) },
     { path: 'strategy', loadChildren: () => import('./../../components/strategy/strategy.module').then(m => m.StrategyModule), canActivate: [AuthGuard]   },
     { path: 'auth', loadChildren: () => import('./../../components/auth/auth.module').then(m => m.AuthModule) },
-    { path: 'signup', loadChildren: () => import('./../../components/signup/signup.module').then(m => m.SignupModule) },
     { path: 'dashboard', loadChildren: () => import('./../../components/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard]  },
     { path: 'admin', loadChildren: () => import('./../../components/admin/admin.module').then(m => m.AdminModule), canActivate: [AuthGuard]   },
     { path: 'gallery', loadChildren: () => import('./../../components/gallery/gallery.module').then(m => m.GalleryModule) },
